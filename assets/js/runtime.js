5f7de8e67be7e10f8de40cf99ee257a8e1c87d5a
Benjamin <pikachuzbj@gamil.com>
2023-05-04 19:24:48 +0800

    修改dialog样式

--- assets/js/runtime.js
+++ assets/js/runtime.js
@@ -130,7 +130,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "155130b8cefec2b65fd6";
+/******/ 	var hotCurrentHash = "fd24005bec2d74d2add2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
